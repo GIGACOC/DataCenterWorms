ce41e57d44e1dfe0ffd198b31547261f3fa92efe
GIGACOC <mike208228@gmail.com>
2026-02-24 16:09:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let input = "";
+const correctCode = "12314155";
 
+function press(number) {
+    input += number;
+    document.getElementById("display").value = input;
+
+    if(input === correctCode) {
+        document.getElementById("message").textContent = "Du hast den Code richtig eingegeben!";
+    } else {
+        document.getElementById("message").textContent = "";
+    }
+}
+
+function clearDisplay() {
+    input = "";
+    document.getElementById("display").value = "";
+    document.getElementById("message").textContent = "";
+}
